aa106b2b96f2356a8e8b3e3bb8e51d33616ec363
Erdal Dogan <erdalsidal99@gmail.com>
2021-01-13 20:04:27 +0300

    get full itinerary

--- Map.py
+++ Map.py
@@ -34,10 +34,19 @@ class Map:
         delta_y = abs(ay - by)
         return sqrt(delta_x ** 2 + delta_y ** 2)
 
+    def follow_backwards(self, came_from, start, end):
+        path: List[Station] = [end]
+        _station = came_from[end]
+        while _station != start:
+            path.append(_station)
+            _station = came_from[_station]
+        path.append(start)
+        return path[::-1]
+
+
     def get_path(self, start: Station, end: Station):
         frontiers = PriorityQueue()
         frontiers.put(start, 0)
-        path: List[Station] = [start]
         came_from: Dict[Station, Optional[Station]] = {start: None}
         cost_so_far: Dict[Station, int] = {start: 0}
 
@@ -47,12 +56,10 @@ class Map:
 
         while not frontiers.empty():
             current: Station = frontiers.get()
-            print("Current: ", current)
             if current == end:
                 print("You have reached to your destination!")
                 break
 
-            print("\tConnections: ", current.connections)
             for c in current.connections:
                 # print("\tC: ", c)
                 # print("\tCost so far: ", cost_so_far[current])
@@ -60,11 +67,9 @@ class Map:
                 # we add 30 because the distance (cost) of moving between two
                 # subway stations are fixed. for out map, it was ~30px between stations
                 if c not in cost_so_far or _cost < cost_so_far[c]:
-                    print("\tin the if ", c)
                     cost_so_far[c] = _cost
                     p = _cost + self.estimate_distance(c, end)
-                    print("\t\tp: ", p)
                     frontiers.put(item=c, priority=p)
                     came_from[c] = current
 
-        return came_from, cost_so_far
+        return self.follow_backwards(came_from, start, end)
