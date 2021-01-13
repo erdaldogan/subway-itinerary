89362de670d41889855eb7536bc34af5b293304a
Erdal Dogan <erdalsidal99@gmail.com>
2021-01-13 20:04:44 +0300

    print path

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from Utils import *
+from Map import Map
 from pprint import pprint
 
 FILE_NAME = "stations.json"
@@ -7,8 +8,13 @@ FILE_NAME = "stations.json"
 def main():
 
     graph = read_stations(FILE_NAME)
+    map = Map(graph)
+
     start = get_user_selection(graph, prompt="Please enter the name of your starting station:")
     end = get_user_selection(graph, prompt="Please enter the name of your ending station:")
+
+    p = map.get_path(start, end)
+    pprint(p)
 
 
 if __name__ == '__main__':
